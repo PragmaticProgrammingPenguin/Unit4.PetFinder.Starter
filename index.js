b55a12f73cea25ef35dba4d0d9d04471fcfc2b1e
Michael Honeycutt <honeycuttmichael5@gmail.com>
2024-08-12 14:33:05 -0400

    Finished with no stretch goal working

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
-
+    // TODO(GET) serve up the public folder as static index.html file
 });
 
 // hello world route
@@ -20,32 +19,30 @@ app.get('/api', (req, res) => {
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
-
+    // res.send("Here is the list of pets: " + [pets[0].name])
+    res.send(pets)
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get('/api/v1/:pets/owner', (req, res) => {
     // get the owner from the request
-
-
+    const owner = pets[req.params.pets].owner
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
-
+    const name = req.params.name
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
